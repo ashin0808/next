52c41aeda34ddb26be5ce36acd4b4484c058cf3a
zhangjinbao <zhangjinbao0808@outlook.com>
2024-09-09 14:55:05 +0800

    feat: change date caching

--- src/app/basketball/firstRound.tsx
+++ src/app/basketball/firstRound.tsx
@@ -14,6 +14,8 @@ type Schedule = {
   visiting_score: number;
 };
 
+export const dynamic = 'force-dynamic'
+
 export default async function FirstRound() {
   const data = await prisma.schedule.findMany({
     where: {
